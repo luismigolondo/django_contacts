0c644be34affad9ef3970dea993e8bcff5d1e483
Luis Miguel Gomez Londono <luismigolondo@gmail.com>
2020-04-08 22:57:26 -0500

    Init

---
+++ envContacto/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/luisgomez/Documents/SkillShare/Django & Python/django_contacts/envContacto/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
